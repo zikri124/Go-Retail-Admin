2e9b6b9b0866719e4bb5bd9daf5710b094ffb797
zikri124 <zikriaizet@gmail.com>
2024-03-09 23:44:10 +0700

    feat(main): set for the /orders Route

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/zikri124/retail-admin-app/internal/handler"
+	"github.com/zikri124/retail-admin-app/internal/infrastructure"
+	"github.com/zikri124/retail-admin-app/internal/repository"
+	"github.com/zikri124/retail-admin-app/internal/router"
+	"github.com/zikri124/retail-admin-app/internal/service"
+)
 
+func main() {
+	g := gin.Default()
+
+	gorm := infrastructure.NewGormPostgres()
+
+	//prepare the order routes group
+	orderRouteGroup := g.Group("/orders")
+	orderRepo := repository.NewOrderRepository(gorm)
+	orderServices := service.NewOrderService(orderRepo)
+	orderHandler := handler.NewOrderHandler(orderServices)
+	orderRouter := router.NewOrderRouter(orderRouteGroup, orderHandler)
+
+	//mount routes group
+	orderRouter.Mount()
+
+	g.Run("127.0.0.1:3000")
 }
