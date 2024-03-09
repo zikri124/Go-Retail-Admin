3abd972d7018b1ca8b991fbb0e0bc96f0598aa10
zikri124 <zikriaizet@gmail.com>
2024-03-10 00:13:46 +0700

    feat(service:order): implement the dto to be the response object from server to client

--- internal/service/order_service.go
+++ internal/service/order_service.go
@@ -3,12 +3,13 @@ package service
 import (
 	"context"
 
-	"github.com/zikri124/retail-admin-app/internal/domain"
+	// "github.com/zikri124/retail-admin-app/internal/domain"
+	"github.com/zikri124/retail-admin-app/internal/dto"
 	"github.com/zikri124/retail-admin-app/internal/repository"
 )
 
 type OrderService interface {
-	GetOrders(ctx context.Context) ([]domain.Order, error)
+	GetOrders(ctx context.Context) ([]dto.OrderDto, error)
 }
 
 type orderServiceImpl struct {
@@ -19,11 +20,19 @@ func NewOrderService(repo repository.OrderQuery) OrderService {
 	return &orderServiceImpl{repo: repo}
 }
 
-func (o *orderServiceImpl) GetOrders(ctx context.Context) ([]domain.Order, error) {
+func (o *orderServiceImpl) GetOrders(ctx context.Context) ([]dto.OrderDto, error) {
 	orders, err := o.repo.GetOrders(ctx)
+
+	ordersDto := []dto.OrderDto{}
+	for _, order := range orders {
+		orderDto := dto.OrderDto{}
+		orderDto.TransformToDto(order)
+		ordersDto = append(ordersDto, orderDto)
+	}
+
 	if err != nil {
 		return nil, err
 	}
 
-	return orders, nil
+	return ordersDto, nil
 }
