de00f197c3cd2e1e24892f57ee64f2592360a663
zikri124 <zikriaizet@gmail.com>
2024-03-09 23:42:30 +0700

    feat(router:order): create order router which contain all handlers to all request in order path

---
+++ internal/router/order_router.go
@@ -0,0 +1,23 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/zikri124/retail-admin-app/internal/handler"
+)
+
+type OrderRouter interface {
+	Mount()
+}
+
+type orderRouterImpl struct {
+	v       *gin.RouterGroup
+	handler handler.OrderHandler
+}
+
+func NewOrderRouter(v *gin.RouterGroup, handler handler.OrderHandler) OrderRouter {
+	return &orderRouterImpl{v: v, handler: handler}
+}
+
+func (o *orderRouterImpl) Mount() {
+	o.v.GET("", o.handler.GetUsers)
+}
